93e0bef3f8eda081a1574acd9b006872fdee8f22
HIDEHIKO KONDO <dev@udonko.net>
2013-04-07 15:20:35 +0900

    Initial Commit

---
+++ TheLastHair/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TheLastHair
+//
+//  Created by HIDEHIKO KONDO on 2013/04/07.
+//  Copyright (c) 2013年 UDONKONET. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TheLastHair/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TheLastHair
+//
+//  Created by HIDEHIKO KONDO on 2013/04/07.
+//  Copyright (c) 2013年 UDONKONET. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
